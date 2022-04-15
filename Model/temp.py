d478ec08fa0a26b5b2ddf160b104634c27dfca4e
pankaj-creator <68644198+pankaj-creator@users.noreply.github.com>
2022-04-15 10:45:09 +0530

    update change

--- Model/temp.py
+++ Model/temp.py
@@ -2,10 +2,15 @@
 """
 Created on Tue Mar  1 16:09:41 2022
 
-@author: ADITYA
+@author: Pankaj
 """
 
 # Implementation of comparision of ARM algorithm
-import filecmp
+import time
 import Apriori_Program
 import main_prog
+import FPGrowth_Program
+
+#print(FPGrowth_Program.start)
+print(Apriori_Program.start)
+print(main_prog.start)
